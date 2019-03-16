66158720d3de47ee95b041063980214c6279229e
Caeden Statia <caeden.s@outlook.com>
2019-03-16 16:47:40 -0700

    Wrap some try catch statements around IncDecSettingsController so it doesnt clog output log when CustomUI instantiates them

---
+++ Beat Saber Utils/Gameplay/HarmonyPatches/IncDecSettingsControllerHeckYou.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine.UI;
+
+namespace BS_Utils.Gameplay.HarmonyPatches
+{
+    [HarmonyPatch(typeof(IncDecSettingsController))]
+    [HarmonyPatch("OnEnable", MethodType.Normal)]
+    class IncDecSettingsControllerHeckYouEnable
+    {
+        static bool Prefix(IncDecSettingsController __instance, Button ___incButton, Button ___decButton)
+        {
+            try
+            {
+                ___incButton.onClick.AddListener(__instance.IncButtonPressed);
+                ___decButton.onClick.AddListener(__instance.DecButtonPressed);
+            }
+            catch { }
+            return false;
+        }
+    }
+
+    [HarmonyPatch("OnDisable", MethodType.Normal)]
+    class IndDecSettingsControllerHeckYouDisable
+    {
+        static bool Prefix(IncDecSettingsController __instance, Button ___incButton, Button ___decButton)
+        {
+            try
+            {
+                ___incButton.onClick.RemoveListener(__instance.IncButtonPressed);
+                ___decButton.onClick.RemoveListener(__instance.DecButtonPressed);
+            }
+            catch { }
+            return false;
+        }
+    }
+}
